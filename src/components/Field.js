2b1a575a7a01b5f69b83f8ff246f4d23a947de9d
vitoriamj <vitoria.m.justino@gmail.com.br>
2022-05-13 14:29:08 -0300

    Criado o component Flag

--- src/components/Field.js
+++ src/components/Field.js
@@ -2,14 +2,16 @@ import React from 'react'; //sempre que tiver JSX tem que ter React
 import {View, StyleSheet, Text} from 'react-native';
 import params from '../params';
 import Mine from './Mine';
+import Flag from './Flag';
 
 export default props => {
-  const {mined, opened, nearMines, exploded} = props;
+  const {mined, opened, nearMines, exploded, flagged} = props;
 
   const styleField = [styles.field];
   if (opened) styleField.push(styles.opened);
   if (exploded) styleField.push(styles.exploded);
-  if (styleField.length === 1) styleField.push(styles.regular);
+  if (flagged) styleField.push(styles.flagged);
+  if (!opened && !exploded) styleField.push(styles.regular);
 
   // cálculo de quantas minas estão presentes dentro da propriedade nearMines
   let color = null;
@@ -28,6 +30,7 @@ export default props => {
         false // se não obedecer as condições, retorna falso e não renderiza nada
       )}
       {mined && opened ? <Mine /> : false}
+      {flagged && !opened ? <Flag /> : false}
     </View>
   );
 };
@@ -59,4 +62,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     borderColor: 'red',
   },
+  flagged: {},
 });
