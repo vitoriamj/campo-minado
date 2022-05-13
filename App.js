c2e4a49367a77765554ea9e585a0019b5b861830
vitoriamj <vitoria.m.justino@gmail.com.br>
2022-05-12 23:56:56 -0300

    Criado os componentes campo e mina

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-
 import params from './src/params';
+import Field from './src/components/Field';
 
 export default class App extends Component {
   render() {
@@ -11,6 +11,15 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           {' '}
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+          <Field />
+          <Field opened />
+          <Field opened nearMines={1} />
+          <Field opened nearMines={2} />
+          <Field opened nearMines={3} />
+          <Field opened nearMines={6} />
+          <Field mined />
+          <Field mined opened />
+          <Field mined opened exploded />
         </Text>
       </View>
     );
